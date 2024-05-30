f3eb991c35c8958021eddcfcdb3781a69beb9006
Dalip Yadav <dalipyadav68@gmail.com>
2024-05-31 04:43:00 +0530

    Imdb clone

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function loading() {
+  return (
+    <div className="flex justify-center mt-16">
+      <Image className="h-52" src="spinner.svg" alt="loading..."></Image>
+    </div>
+  );
+}
